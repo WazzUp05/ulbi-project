520e94643906c5e7020862599f536027041de3ae
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2023-03-10 20:05:45 +0600

    fix tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        jest: true,
-    },
+    env: { browser: true, es2021: true, jest: true },
     extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
     parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
+    parserOptions: { ecmaFeatures: { jsx: true }, ecmaVersion: 'latest', sourceType: 'module' },
     plugins: ['react', '@typescript-eslint', 'i18next'],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -32,25 +22,15 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'arrow-parens': ['warn', 'as-needed'],
         'object-curly-newline': ['warn', 'never'],
-        'wrap-iife': ['warn', 'never'],
+        'wrap-iife': ['warn', 'inside'],
         'i18next/no-literal-string': [
             'error',
-            {
-                markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to'],
-            },
+            { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
-    globals: {
-        __IS_DEV__: true,
-    },
+    globals: { __IS_DEV__: true },
     overrides: [
-        {
-            files: ['**/src/**/*.test.{ts,tsx}'],
-            rules: {
-                'i18next/no-literal-string': 'off',
-            },
-        },
+        { files: ['**/src/**/*.test.{ts,tsx}'], rules: { 'i18next/no-literal-string': 'off' } },
     ],
 };
