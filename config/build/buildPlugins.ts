c4f7bc06686cfe39cf12e04d5aa5f0bbea4fe4b8
WazzUp05 <vasyaribakov1@gmail.com>
2023-04-02 13:07:47 +0600

    add test for login module

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,10 +15,9 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({ __IS_DEV__: JSON.stringify(isDev) }),
     ];
 
-    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
-
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
     }
 
     return plugins;
